12e6b1256f58d990a124f973686c37553197f74d
White Shadow <82247249+Ravi-Sapkota@users.noreply.github.com>
2022-04-04 11:10:45 +0545

    Added code to Lab_3_3.cpp

--- Lab_3_3.cpp
+++ Lab_3_3.cpp
@@ -1,1 +1,20 @@
 // Write a program showing basic to class type conversion
+
+#include <iostream>
+
+class Time {
+public:
+  int hr, min;
+  Time(int x) {
+    hr = x / 60;
+    min = x % 60;
+  }
+  void display() { std::cout << "Time = " << hr << ":" << min << "\n"; }
+};
+
+int main() {
+  int duration = 150;
+  Time t = duration;
+  t.display();
+  return 0;
+}
